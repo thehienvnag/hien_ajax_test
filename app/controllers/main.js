3641fd6c2895bc2bad2719087c63f6b359ca4478
HienHT <thehienvnag>
2019-06-02 17:08:35 +0700

    conflicts solved

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -6,11 +6,8 @@ $(document).ready(function() {
   function test() {}
   function newBranch() {}
   function search() {
-<<<<<<< HEAD
     console.log("aloooo");
-=======
     console.log("alods");
->>>>>>> HienHT
   }
 
   function layDanhSachNguoiDung() {
